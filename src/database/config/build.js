40384a3041f41f0a058bb8689fb31a3c16c7a55a
afahgan <afahgan@students.iugaza.edu.ps>
2022-03-09 19:10:45 +0200

    Update files

--- src/database/config/build.js
+++ src/database/config/build.js
@@ -4,7 +4,8 @@ const connection = require('./connection');
 
 const dbBuild = () => {
   const sql = readFileSync(join(__dirname, 'build_test.sql')).toString();
-  return connection.query(sql);
+  return connection.query(sql).then(() => console.log('build created successfully!'))
+    .catch((e) => console.error('failed to build', e.stack));
 };
-
+dbBuild();
 module.exports = dbBuild;
