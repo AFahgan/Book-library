a7398891d11ee9f76e8317d9f36abe2c2a8d0986
KAHMOOSHA <brhmhelou@gmail.com>
2022-03-09 11:07:30 +0200

    create dom

---
+++ Public/Js/index.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-undef */
+fetch('/getBook')
+  .then((res) => res.json())
+  .then((books) => {
+    // eslint-disable-next-line no-console
+    console.log(books);
+    const bookContainer = document.querySelector('.books-cont');
+    books.forEach((book) => {
+      const bookdiv = document.createElement('div');
+      bookdiv.classList.add('book');
+      bookContainer.appendChild(bookdiv);
+      const bookImg = document.createElement('img');
+      bookImg.classList.add('book-img');
+      bookImg.src = book.cover;
+      bookdiv.appendChild(bookImg);
+      const bookName = document.createElement('h4');
+      bookName.classList.add('book-name');
+      bookName.textContent = book.name;
+      bookdiv.appendChild(bookName);
+      const bookA = document.createElement('a');
+      bookA.href = './addbook.html';
+      const bookBtn = document.createElement('button');
+      bookBtn.classList.add('book-btn');
+      bookA.appendChild(bookBtn);
+      bookdiv.appendChild(bookA);
+    });
+  });
